19d3ae3c4cf2f4f544dccd2759ecc5d513d79eb6
Rashawn <rjcoolsav1@gmail.com>
2026-02-10 17:50:22 -0600

    finsihed

--- project_work.py
+++ project_work.py
@@ -1,61 +1,53 @@
 from data.utils import read_csv
-
 STEP_THRESHOLD = 120
-
 
 def get_changes(magnitudes):
     """Calculate changes between consecutive values.
-
     Args:
         magnitudes: A list of acceleration readings in chronological order.
     Returns:
         A list containing the change in acceleration between each pair of consecutive readings.
-    
     NOTE: Your return list should always be one element smaller than the input list. Why is that?
-    EXAMPLE: if magnitudes = [100, 200, 50, 100], this function should return [100, -150, 50]
+    EXAMPLE: if magnitudes = [100, 200, 50, 100], this function should return [100, "-150," 50]
     """
-
     changes = []
-
     # TODO: For each pair, calculate: next_value - current_value
-
+    for i in range(len(magnitudes) - 1):
+        change = magnitudes[i+1] - magnitudes[i]
+        changes.append(change)
     return changes
-
 
 def count_peaks(changes, threshold):
     """Count how many changes are above threshold.
-
     Args:
         changes: A list of changes in acceleration in chronological order.
         threshold: A number above which we consider an acceleration change to indicate a step.
     Returns:
         The number of acceleration changes greater than the threshold.
-    
     EXAMPLE: if changes = [800, 100, 900, 400] and threshold = 700, this function should return 2.
     """
-
     step_count = 0
-
     # TODO: Count changes greater than or equal to threshold
-
+    for change in changes:
+        if change >= threshold:
+            step_count += 1
     return step_count
-
 
 def count_steps(magnitudes, threshold=STEP_THRESHOLD):
     """Main function: use other functions to count steps.
-    
     Args:
         magnitudes: A list of acceleration readings in chronological order.
         threshold: the minimum acceleration change that is considered a step.
-
     Returns:
         The number of steps the user took.
     """
-
     # TODO: Use get_changes() and count_peaks()
-
-    return 0
-
+    if not magnitudes:
+        return 0
+        
+    changes = get_changes(magnitudes)
+    step_count = count_peaks(changes, threshold)
+    return step_count
 
 if __name__ == "__main__":
     try:
